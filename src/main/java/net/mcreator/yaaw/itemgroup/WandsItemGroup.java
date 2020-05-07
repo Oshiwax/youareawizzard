d9dee1881ffea7ac61e16b17fb9ebca9dedbb568
Maxime <Maxime@DESKTOP-3HCCIE2>
2020-05-07 18:46:18 +0200

    -Butter Beer -Wands Category

--- src/main/java/net/mcreator/yaaw/itemgroup/WandsItemGroup.java
+++ src/main/java/net/mcreator/yaaw/itemgroup/WandsItemGroup.java
@@ -11,18 +11,18 @@ import net.minecraft.item.ItemGroup;
 import net.mcreator.yaaw.YaawElements;
 
 @YaawElements.ModElement.Tag
-public class YaawItemsItemGroup extends YaawElements.ModElement {
-	public YaawItemsItemGroup(YaawElements instance) {
-		super(instance, 1);
+public class WandsItemGroup extends YaawElements.ModElement {
+	public WandsItemGroup(YaawElements instance) {
+		super(instance, 3);
 	}
 
 	@Override
 	public void initElements() {
-		tab = new ItemGroup("tabyaawitems") {
+		tab = new ItemGroup("tabwands") {
 			@OnlyIn(Dist.CLIENT)
 			@Override
 			public ItemStack createIcon() {
-				return new ItemStack(Items.DEBUG_STICK, (int) (1));
+				return new ItemStack(Items.STICK, (int) (1));
 			}
 
 			@OnlyIn(Dist.CLIENT)
